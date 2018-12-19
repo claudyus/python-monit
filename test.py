bd2a42aa3dfc078955090cb258f694d27b25f3c0
Claudio Mignanti <c.mignanti@gmail.com>
2018-12-19 12:21:32 +0100

    test: use global mon object

--- test.py
+++ test.py
@@ -7,26 +7,24 @@ import requests_mock
 import unittest
 from monit import Monit
 
-@requests_mock.Mocker()
 class TestMonitClass(unittest.TestCase):
-    text = ''
+    mon = None
 
-    def setUp(self):
+    @requests_mock.Mocker()
+    def setUp(self, mock):
         with open('tests/fixture1.xml', 'r') as file:
             self.text = file.read().replace('\n', '')
+            mock.get('http://localhost:2812/_status', text=self.text)
+            self.mon = Monit()
 
-    def test_process(self, mock):
-        mock.get('http://localhost:2812/_status', text=self.text)
-        mon = Monit()
-        assert "type" in mon['agent-1'].__dict__
-        assert mon['agent-1'].type == "process"
+    def test_process(self):
+        assert "type" in self.mon['agent-1'].__dict__
+        assert self.mon['agent-1'].type == "process"
 
-    def test_platform(self, mock):
-        mock.get('http://localhost:2812/_status', text=self.text)
-        mon = Monit()
-        assert  mon['platform'].name == 'Linux'
-        assert  mon['platform'].release == '3.12.70'
-        assert  'memory' in mon['platform'].__dict__
+    def test_platform(self):
+        assert  self.mon['platform'].name == 'Linux'
+        assert  self.mon['platform'].release == '3.12.70'
+        assert  'memory' in self.mon['platform'].__dict__
 
 if __name__ == '__main__':
     unittest.main()
