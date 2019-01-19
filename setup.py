ab18da43ad02ebc0e09fc077d811081fd7ce25c5
Claudio Mignanti <c.mignanti@gmail.com>
2019-01-19 09:09:08 +0100

    Update setup.py; pipy now should point to my repo

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ setup(
     name='monit',
     description='Interface to the Monit system manager and monitor (http://mmonit.com/monit/)',
     version='0.4',
-    author='Camilo Polymeris',
-    author_email='cpolymeris@gmail.com',
-    url='https://github.com/polymeris/python-monit',
+    author='Camilo Polymeris, Claudio Mignanti',
+    author_email='cpolymeris@gmail.com, c.mignanti@gmail.com',
+    url='https://github.com/claudyus/python-monit',
     py_modules=['monit'],
     requires=['requests']
     )
