fda31bc7bc4975d9b863542cadf0087dd68611ae
Claudio Mignanti <c.mignanti@gmail.com>
2019-01-19 09:10:50 +0100

    Release version 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='monit',
     description='Interface to the Monit system manager and monitor (http://mmonit.com/monit/)',
-    version='0.4',
+    version='0.5',
     author='Camilo Polymeris, Claudio Mignanti',
     author_email='cpolymeris@gmail.com, c.mignanti@gmail.com',
     url='https://github.com/claudyus/python-monit',
